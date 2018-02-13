c82041ed5ef5c91dd9d202098197f4917504a280
Michael Durfey <mdurfey@me.com>
2018-02-13 12:32:53 -0800

    server and basic setup complete

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,15 @@
+var submitButton = document.getElementById('submitButton');
+var textArea = document.getElementById('textArea');
 
-
+submitButton.addEventListener('click', event => {
+  event.preventDefault();
+  // console.log(event.path[1][0].value);
+  let input = event.path[1][0].value;
+  console.log(input, typeof input);
+  $.post('/', input, function(err, data){
+    if (err){
+      console.log(err);
+    }
+    console.log('post success!!')
+  })
+})
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,22 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
+app.use(express.static(__dirname + '/client'))
+app.use(express.static(__dirname))
+app.use(bodyParser.json());
 
+app.get('/', res => {
+  res.writeHead(200);
+  res.send('/index.html');
+})
+
+app.post('/', (req, res) => {
+ console.log('post!');
+ console.log(req.body);
+ res.end();
+})
+
+app.listen(3000, function(){
+  console.log('server listening on port 3000 !!')
+})
