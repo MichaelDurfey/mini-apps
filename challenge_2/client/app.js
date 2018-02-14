f6706eb44fc3b7e74ac81a342b8a289aa87b4664
Michael Durfey <mdurfey@me.com>
2018-02-13 18:35:21 -0800

    bare minimums complete

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,15 +1,67 @@
 var submitButton = document.getElementById('submitButton');
 var textArea = document.getElementById('textArea');
 
+class GetCSV {
+  constructor(text){
+    this.currentText = [];
+    this.columns = [];
+  }
+
+  post(data){
+    let that = this;
+    $.ajax({
+      type: "POST",
+      url: '/',
+      data: data,
+      success: function (success){
+        that.currentText = success;
+        that.convertData();
+      },
+      contentType: 'application/json',
+    })
+  }
+
+  convertData(current){
+    current = current || JSON.parse(this.currentText);
+    //iterate through each object and extract values and contruct table
+      //make table row
+      console.log(current);
+      let headers = current.keys.slice(0, -1)
+      let values = current.values.split(',');
+
+      let people = [];
+      for (let i = 0; i< values.length; i += 6){
+        people.push(values.slice(i, i+6))
+      }
+
+      let headerRow = document.createElement('tr');
+      headers.forEach( item => {
+        let header = document.createElement('th');
+        header.innerHTML = item;
+        headerRow.appendChild(header);
+      })
+      document.getElementById('table').appendChild(headerRow)
+
+      people.forEach( item => {
+        let tr = document.createElement('tr');
+        item.forEach( arr => {
+          let td = document.createElement('td');
+          td.innerHTML = arr;
+          tr.appendChild(td)
+        })
+        document.getElementById('table').appendChild(tr);
+      })
+  } //END CONVERT DATA
+
+} /// END CLASS
+
+let currentCSV = new GetCSV();
+
 submitButton.addEventListener('click', event => {
   event.preventDefault();
-  // console.log(event.path[1][0].value);
-  let input = event.path[1][0].value;
-  console.log(input, typeof input);
-  $.post('/', input, function(err, data){
-    if (err){
-      console.log(err);
-    }
-    console.log('post success!!')
-  })
+  let data = {
+    input: event.path[1][0].value.replace(/[;]/, '')
+  };
+  data = JSON.stringify(data);
+  currentCSV.post(data);
 })
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,19 +2,38 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + '/client'))
-app.use(express.static(__dirname))
+app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
 app.get('/', res => {
   res.writeHead(200);
+  console.log('get!')
   res.send('/index.html');
 })
 
 app.post('/', (req, res) => {
- console.log('post!');
- console.log(req.body);
- res.end();
+  let input = req.body.input;
+  input = [JSON.parse(input)];
+  let keys = Object.keys(input[0]);
+  let values = [];
+ let mapVals = (currentNode, result = '') => {
+    for (var k in currentNode) {
+      if (typeof currentNode[k] !== 'object' && currentNode[k] !== null){
+      values.push(currentNode[k])
+      } else {
+        mapVals(currentNode[k]);
+      }
+    }
+  };
+  mapVals(input);
+  values = values.join(',')
+  let data = {
+    keys: keys,
+    values: values
+  }
+  data = JSON.stringify(data);
+  res.send(data);
 })
 
 app.listen(3000, function(){
